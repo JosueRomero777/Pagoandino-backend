b3358533d7afac16183f4c35c484d96909daf007
JosueRomero777 <rimero782@gmail.com>
2025-05-08 09:37:15 -0500

    estructura de recursos y servicio de prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './resources/auth/auth.module';
+import { UserModule } from './resources/user/user.module';
+import { CustomerModule } from './resources/customer/customer.module';
+import { ProductModule } from './resources/product/product.module';
+import { OrderModule } from './resources/order/order.module';
+import { InvoiceModule } from './resources/invoice/invoice.module';
+import { PaymentModule } from './resources/payment/payment.module';
+import { SubscriptionModule } from './resources/subscription/subscription.module';
+import { CommonModule } from './resources/common/common.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, CustomerModule, ProductModule, OrderModule, InvoiceModule, PaymentModule, SubscriptionModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
